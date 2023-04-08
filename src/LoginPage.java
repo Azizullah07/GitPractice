3db740746e1e7f212e431e268b171e71bee31a86
azizafghan226@gmail.com <azizafghan226@gmail.com>
2023-04-08 12:50:35 -0400

    login page code

---
+++ src/LoginPage.java
@@ -0,0 +1,11 @@
+public class LoginPage {
+    public static void main(String[] args) {
+
+
+        String username = "Noor" ;
+        String password = "pass123" ;
+
+
+        System.out.println("userName " + username + "password is " + password);
+    }
+}
